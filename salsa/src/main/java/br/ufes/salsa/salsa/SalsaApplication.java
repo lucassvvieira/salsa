77072572bc9ac7ab966692d673529f6f7a2b7a79
emethium <nikolas.serafini@gmail.com>
2017-08-30 10:23:52 -0300

    Created new spring boot project

---
+++ salsa/src/main/java/br/ufes/salsa/salsa/SalsaApplication.java
@@ -0,0 +1,12 @@
+package br.ufes.salsa.salsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalsaApplication.class, args);
+	}
+}
